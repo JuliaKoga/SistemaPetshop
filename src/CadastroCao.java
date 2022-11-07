ac476bfb9ae3cc7a86faa5a44a0ebebf256e9bf9
JuliaKoga <112908764+JuliaKoga@users.noreply.github.com>
2022-11-07 20:25:52 -0300

    setando IDs pela classe teste

--- src/CadastroCao.java
+++ src/CadastroCao.java
@@ -2,9 +2,15 @@ public class CadastroCao {
 
     private Cachorro[] cachorrosCadastrados = new Cachorro[10];
 
-    private static int i = 0;
+    private static int sequence = 1;
+    private int i = 0;
 
     public void cadastrar (Cachorro cachorro){
+        cachorro.setId(sequence);
+        cachorro.getDono().setId(sequence);
+        cachorro.getDono().getEndereco().setId(sequence);
+        sequence++;
+
         if(cachorro!=null){
             cachorrosCadastrados[i] = cachorro;
             i++;
